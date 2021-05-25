9b7bcd6b2bd642b9ddbad62206db1a3dbde0b553
Vic <61720168+vicsamuel@users.noreply.github.com>
2021-05-25 14:59:39 -0400

    More challenges completed

---
+++ Java Currency Formatter.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Solution {
+    
+    public static void main(String[] args) {
+        /* Read input */
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+        
+        // New India Locale
+        Locale indiaLocale = new Locale("en", "IN");
+
+        NumberFormat us     = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat india  = NumberFormat.getCurrencyInstance(indiaLocale);
+        NumberFormat china  = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+        /* Print output */        
+        System.out.println("US: "     + us.format(payment));
+        System.out.println("India: "  + india.format(payment));
+        System.out.println("China: "  + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+    }
+}
