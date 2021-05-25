b737a1ab1042eff5c72c1eae22b5a65746c20e86
Vic <61720168+vicsamuel@users.noreply.github.com>
2021-05-25 14:21:44 -0400

    First few challenges

---
+++ Java Loops II.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int c = a;
+            for(int j=0;j<n;j++){
+                c += Math.pow(2, j)*b;
+                System.out.printf("%s ",c);
+            }
+            System.out.println();
+        }
+        in.close();  
+    }
+}
