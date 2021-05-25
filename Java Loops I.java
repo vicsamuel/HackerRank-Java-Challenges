70e905e9d2f4ed93add4cdf56d8f313800a59fea
Vic <61720168+vicsamuel@users.noreply.github.com>
2021-05-25 14:24:00 -0400

    Formatting

--- Java Loops I.java
+++ Java Loops I.java
@@ -10,12 +10,12 @@ import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
-
-        for(int i = 1; i <= 10; i++){
-
+        for(int i = 1; i <= 10; i++)
+        {
             System.out.printf("%d x %d = %d%n", N, i, N*i);
         }
     }
