50ac670e4aadc8474d174599af0bd7e3c52396f5
Vic <61720168+vicsamuel@users.noreply.github.com>
2021-05-27 20:40:20 -0400

    More challenges

---
+++ Java BigDecimal.java
@@ -0,0 +1,38 @@
+import java.math.BigDecimal;
+import java.util.*;
+class Solution{
+
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+      	sc.close();
+        
+        // Comparator to sort while preserving contents of string
+        Comparator<String> comp = new Comparator<String>() 
+        {
+            @Override
+            public int compare (String s1, String s2) 
+            {
+                BigDecimal a = new BigDecimal(s1);
+                BigDecimal b = new BigDecimal(s2);
+                
+                // This will sort descending
+                return b.compareTo(a);
+            }       
+        };
+        
+        Arrays.sort(s, 0, n, comp);
+        
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+
+}
